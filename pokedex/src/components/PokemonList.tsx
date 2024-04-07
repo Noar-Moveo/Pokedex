4277d5c0ac984d8043f34044b62fdb7df799c22e
Noa Reuven <noar@moveo.co.il>
2024-04-07 21:53:32 +0300

    add interface

---
+++ pokedex/src/components/PokemonList.tsx
@@ -0,0 +1,19 @@
+//import React from "react";
+import { usePokemonData } from '../hooks/usePokemonData.ts';
+
+const PokemonList: React.FC = () => {
+    const { data: pokemonList, loading, error } = usePokemonData();
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>;
+
+    return (
+        <div>
+          {pokemonList.map((pokemon) => ( 
+            <div key={pokemon.name}>{pokemon.name}</div>
+          ))}
+        </div>
+      );
+    };
+    
+    export default PokemonList;
--- pokedex/src/hooks/usePokemonData.ts
+++ pokedex/src/hooks/usePokemonData.ts
@@ -1,9 +1,20 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+interface Pokemon {
+    name: string;
+    url: string;
+  }
+  
+  interface UsePokemonDataReturn {
+    data: Pokemon[];
+    loading: boolean;
+    error: Error | null;
+  }
+
 const API_URL = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
-export const usePokemonData = () => {
+export const usePokemonData = (): UsePokemonDataReturn => {
     const [data, setData] = useState<any[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<Error | null>(null);
