61ebd1e0652b9d637a50f2d761f4be45ede0e477
Noa Reuven <noar@moveo.co.il>
2024-04-08 10:13:24 +0300

    without StyledComponent

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -1,32 +1,12 @@
-//import React from 'react';
-import PokemonList from './components/PokemonList';
-import styled from 'styled-components';
-
-const AppContainer = styled.div`
-  text-align: center;
-  background-color: #ffffff;
-  color: #373299
-`;
-
-// a styled component for the main heading
-const MainHeading = styled.h1`
-  color: #373299; 
-`;
-
-const StyledPokemonList = styled.div`
-  color: #373299; 
-`;
+import PokemonList from './components/PokemonList'
 
 function App() {
   return (
-    <AppContainer className="App">
-      <MainHeading>Pokedex</MainHeading>
-      <StyledPokemonList>
+    <div>
+      <h1>Pokedex</h1>
       <PokemonList />
-      </StyledPokemonList>
-    </AppContainer>
+    </div>
   );
 }
 
 export default App;
-
